b85c4f4bd1979ee0d1be49d0eed1a1b05ebf0add
Patrick Weppelmann <patrick.weppelmann+KBGroup@neoimpulse.de>
2026-03-24 12:10:33 +0100

    Made application mor safe

--- client/src/main.js
+++ client/src/main.js
@@ -2,6 +2,16 @@
  * Entry point – loads styles and starts the app.
  */
 
+/* Self-hosted (CSP: keine fonts.googleapis.com) */
+import "@fontsource/outfit/300.css";
+import "@fontsource/outfit/400.css";
+import "@fontsource/outfit/500.css";
+import "@fontsource/outfit/600.css";
+import "@fontsource/outfit/700.css";
+import "@fontsource/space-grotesk/400.css";
+import "@fontsource/space-grotesk/500.css";
+import "@fontsource/space-grotesk/600.css";
+import "@fontsource/space-grotesk/700.css";
 import "./style.css";
 import "./participant-overlay.css";
 import { bootstrap } from "./app/index.js";
